ffaec2d73d644c9bc98321f5b4390a5bbfe65131
ilyasmohd <md.ilyas89@gmail.com>
2026-05-10 15:42:31 +0300

    initial commit

---
+++ api-caller-v1.py
@@ -0,0 +1,10 @@
+import google.generativeai as genai
+from PIL import Image
+
+genai.configure(api_key="AIzaSyBkSiqJ7RS_x9HDxIDsLkDYqZoiehxORXA")
+model = genai.GenerativeModel('gemini-2.5-flash')
+
+img = Image.open('chart_screenshot.JPG')
+response = model.generate_content(["Is this stock chart showing a breakout? Give me a Buy/Sell verdict.", img])
+
+print(response.text)
